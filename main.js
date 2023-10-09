4da9bd8b39458c7c3976cefafe02d784dd7a091c
Ali Siddiqui <alisiddiqui5840@gmail.com>
2023-10-09 16:58:59 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var type = new Typed(".multiple-text", {
+    strings : [ "FrontEnd Developer" , "Youtuber" , "Blogger"],
+    typeSpeed: 100,
+    backspeed:100,
+    backDelay: 1000,
+    loop: true
+})
+
+
