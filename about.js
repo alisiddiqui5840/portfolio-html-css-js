a6c0afb0f13977fa0fded6ed4d9f3e7df6167e9b
alisiddiqui5840 <91695748+alisiddiqui5840@users.noreply.github.com>
2023-10-12 18:00:21 +0500

    Add files via upload

---
+++ about.js
@@ -0,0 +1,20 @@
+//about js
+console.log(window);
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+	const triggerBottom = window.innerHeight / 5 * 4;
+	boxes.forEach((box, idx) => {
+		const boxTop = box.getBoundingClientRect().top;
+		
+		if(boxTop < triggerBottom) {
+			box.classList.add('show');
+		} else {
+			box.classList.remove('show');
+		}
+	});
+}
